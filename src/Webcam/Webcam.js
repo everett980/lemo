f44162251102c261393e7ed8dc1086bb03ebe0bc
Everett Ross <everett980@Everetts-MacBook-Air.local>
2017-05-13 18:31:19 -0400

    adds start of webcam work

--- src/Webcam/Webcam.js
+++ src/Webcam/Webcam.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Webcam as ReactWebcam } from 'react-webcam';
 
-class GameStart extends Component {
+export default class Webcam extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <h1>{this.state.word}</h1>
+          <h1>{this.props.word}</h1>
         </div>
+        <ReactWebcam />
         {/* webcam footage goes fullcreen here */}
       </div>
     );
   }
 }
-
-export default App;
