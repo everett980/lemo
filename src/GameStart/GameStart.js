7ae1fd1aabb18a26f74b3b0d10581d8bb206658c
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-13 18:47:01 -0400

    adds url string randomizer

--- src/GameStart/GameStart.js
+++ src/GameStart/GameStart.js
@@ -4,6 +4,7 @@ export default class GameStart extends Component {
   constructor(props) {
     super(props);
     this.state = { number: 5, leader: 'lily' };
+    this.urlStart ='';
   }
 
   render() {
@@ -13,7 +14,12 @@ export default class GameStart extends Component {
         <div className="App-header">
           <h1>{this.state.number} people are here</h1>
         </div>
-        { this.state.leader ? <button onClick={this.props.sendChat}>Start</button> : null }
+        <div className="App-body">
+          { this.state.leader && <button onClick={this.props.sendChat}>Start</button> }
+          { this.state.leader &&
+              <p>Share this url with your friends!</p>
+              <p>`${ this.urlStart }/Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5)`</p>
+          }</div>
       </div>
     );
   }
