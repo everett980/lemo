b4c71b3bf5236bd66f347bd4b7d7a34680f02aff
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-14 09:01:13 -0400

    fix some bugs

--- server/index.js
+++ server/index.js
@@ -30,7 +30,8 @@ io.on('connection', function(socket) {
   console.log(playerIds);
 
   function scoreDifference(previousEmotion, currentEmotion) {
-    console.log( Math.min(previousEmotion, currentEmotion), Math.max(previousEmotion, currentEmotion))
+    console.log("to divide", Math.min(previousEmotion, currentEmotion), Math.max(previousEmotion, currentEmotion))
+    console.log("divided", Math.min(previousEmotion, currentEmotion) / Math.max(previousEmotion, currentEmotion) || 1)
     return Math.min(previousEmotion, currentEmotion) / Math.max(previousEmotion, currentEmotion);
   }
 
