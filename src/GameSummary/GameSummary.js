ee1b4a1689e4fa1f81bfe13a305c0182c1e1c12e
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-14 07:37:24 -0400

    add styling to last page

--- src/GameSummary/GameSummary.js
+++ src/GameSummary/GameSummary.js
@@ -16,11 +16,21 @@ class GameSummary extends Component {
     const images = this.props.resultsArr.map(([imgUri]) => <img src={imgUri} />);
     return (
       <div className="App">
-        <div> 
-          The gyame has Ended. Press Start to Start Again
-          <button className="btn waves-effect waves-indigo" onClick={ this.restartGame }>Start</button> 
-          {images}
-        </div> 
+        <div className="row">
+          <div className="col s12">
+            <p className="App-intro">good job you!</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            {images}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <button className="btn waves-effect waves-indigo" onClick={ this.restartGame }>again?</button> 
+          </div>
+        </div>
       </div>
     );
   }
